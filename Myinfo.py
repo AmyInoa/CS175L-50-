a30b90fadf83c9ffee9cbed7e56c2d4d7073b22e
AmyInoa <91210803+AmyInoa@users.noreply.github.com>
2022-01-24 19:10:22 -0500

    Add files via upload

--- Myinfo.py
+++ Myinfo.py
@@ -2,4 +2,7 @@
 #Amy Inoa
 #Myinfo
 print ("My name is Amy Inoa")
-print ( "I am a Student")
+print ("I Live at 206 Bloomfield ave,West Caldwell,New Jersey,07006")
+print ("My telephone number is 973-207-5674")
+print ("My major is computer science")
+
