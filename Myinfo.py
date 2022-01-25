c329047775b011d1592b27f11fb2a0291e679e29
AmyInoa <91210803+AmyInoa@users.noreply.github.com>
2022-01-24 19:11:40 -0500

    Delete Myinfo.py

--- Myinfo.py
+++
@@ -1,8 +0,0 @@
-#CS175-50
-#Amy Inoa
-#Myinfo
-print ("My name is Amy Inoa")
-print ("I Live at 206 Bloomfield ave,West Caldwell,New Jersey,07006")
-print ("My telephone number is 973-207-5674")
-print ("My major is computer science")
-
