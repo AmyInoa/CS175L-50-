d9795d4b31a37eee4e1cca5375461bedd9075b29
AmyInoa <91210803+AmyInoa@users.noreply.github.com>
2022-03-30 17:30:01 -0400

    average from input

---
+++ average from input.py
@@ -0,0 +1,19 @@
+#Amy Inoa
+#CS 175L
+#Average from input
+
+def main():
+    file_num = open("numbers.txt" ,'r')
+    x = 0
+    total = 0
+    for line in file_num:
+        x = x +1
+        amount = float(line)
+        total = total + amount
+        print(f"I read in {x} number(s) "
+              f"Current number is: {amount:.1f}"
+              f"Total is: {total:.1f}")
+    file_num.close()
+    average = total / x
+    print (f"Average: {average:.1f}")
+main()
